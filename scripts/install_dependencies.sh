2fd3d72cc135c4644b41b87c7c49a9980892d565
Khalid Bin Sattar <42968874+khalids226@users.noreply.github.com>
2024-01-11 01:39:27 +0100

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,2 @@
 !/bin/bash
-if ! [ -x "$(command -v httpd)" ]; then sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2 >&2;   exit 1; fi # install apache if not already installed
-
+if ! [ -x "$(command -v httpd)" ]; then sudo amazon-linux-extras install -y lamp-mariadb10.2-php8.2 php8.2 >&2;   exit 1; fi # install apache if not already installed
