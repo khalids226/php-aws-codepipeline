370ec856fa823090873b860d916e7f6437d6d980
Khalid Bin Sattar <42968874+khalids226@users.noreply.github.com>
2024-02-17 06:27:27 +0100

    Update install_composer_dependencies.sh
    update the code with latest command line

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /var/www/html
-sudo yum install wget -y
-sudo wget https://getcomposer.org/composer.phar
-sudo php composer.phar install
+sudo yum install wget php-cli php-json -y  # Install wget and required PHP packages
+sudo wget https://getcomposer.org/composer.phar -O /usr/local/bin/composer  # Download Composer
+sudo chmod +x /usr/local/bin/composer  # Make Composer executable system-wide
+sudo composer install  # Use Composer to install project dependencies
