e2040969644bbb3352ef5e157d7952df6097c25b
Khalid Bin Sattar <42968874+khalids226@users.noreply.github.com>
2026-02-02 18:33:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 
-This is a Test for the DevOps Pipeline, tested by Khalid. reviewed by Khalid himself. Hello from Khalid to all the Multicloud DevOps Batch 5 students. 
-
-This is to certify that the code-deploy is working fine.
 
 Hi from Devskill Multicloud DevOps Batch 5 
 
