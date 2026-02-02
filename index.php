8562e7eacef23916faee1873ace3dfcdffea2f63
Khalid Bin Sattar <42968874+khalids226@users.noreply.github.com>
2026-02-02 18:13:31 +0100

    Update greetings for Multicloud DevOps Batch 5

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
-This is a Test for the DevOps Pipeline, tested by Khalid. reviewed by Khalid himself. Hello from Khalid to all the AWS DevOps Gen AI students. 
+This is a Test for the DevOps Pipeline, tested by Khalid. reviewed by Khalid himself. Hello from Khalid to all the Multicloud DevOps Batch 5 students. 
 
 This is to certify that the code-deploy is working fine.
 
-Hi from Devskill AWS DevOps Gen AI Batch 1 
+Hi from Devskill Multicloud DevOps Batch 5 
 
